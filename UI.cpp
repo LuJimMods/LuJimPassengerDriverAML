0b7485e80c07b605a364b82dc2550f7ada45af2c
LuJim <107133733+LuJimMods@users.noreply.github.com>
2026-06-25 15:17:42 -0300

    Delete UI.cpp

--- UI.cpp
+++
@@ -1,6 +0,0 @@
-#include "UI.h"
-#include "Log.h"
-namespace UI {
-void ShowMessage(const char* msg) { LPD_LOGI("MESSAGE: %s", msg ? msg : ""); }
-void DrawProgress(float progress) { (void)progress; /* TODO: desenhar barra via HUD/RenderWare hook */ }
-}
