4c404cca7dc63f0a5d8c50ffc5bd86a73e1526f6
LuJim <107133733+LuJimMods@users.noreply.github.com>
2026-06-25 15:16:34 -0300

    Delete Notifications.h

--- Notifications.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-class Notifications {
-public:
-    static void Show(const char* text);
-    static void Enabled();
-    static void Disabled();
-    static void NoRecruit();
-    static void Progress(float percent, bool enabling);
-};
