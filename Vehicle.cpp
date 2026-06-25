2b83900baa921c178580604b8e5f6b4a4a2d97ba
LuJim <107133733+LuJimMods@users.noreply.github.com>
2026-06-25 15:18:07 -0300

    Delete Vehicle.cpp

--- Vehicle.cpp
+++
@@ -1,7 +0,0 @@
-#include "Vehicle.h"
-#include "Log.h"
-void* Vehicle::FindTargetVehicle(){
-    LPD_Log("[VEHICLE] Busca de veículo ainda bloqueada no SafeTest.");
-    return nullptr;
-}
-bool Vehicle::IsAllowed(void*){ return false; }
