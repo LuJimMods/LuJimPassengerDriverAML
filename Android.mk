ba5e1bb3fbcff0e42579dd211d7b5bdb683b5a8a
LuJim <107133733+LuJimMods@users.noreply.github.com>
2026-06-25 14:57:43 -0300

    Refactor Android.mk for improved readability

--- Android.mk
+++ Android.mk
@@ -1,14 +1,11 @@
 LOCAL_PATH := $(call my-dir)
 
 include $(CLEAR_VARS)
-LOCAL_CPP_EXTENSION := .cpp .cc
-LOCAL_CPP_FEATURES += exceptions
 
 LOCAL_MODULE := LuJimPassengerDriver
+
 LOCAL_SRC_FILES := \
     main.cpp \
-    mod/logger.cpp \
-    mod/config.cpp \
     Config.cpp \
     Log.cpp \
     Notifications.cpp \
@@ -18,8 +15,20 @@ LOCAL_SRC_FILES := \
     Vehicle.cpp \
     PassengerDriver.cpp
 
-LOCAL_C_INCLUDES := $(LOCAL_PATH)
-LOCAL_CPPFLAGS := -std=c++17 -fexceptions -frtti -Wall -Wextra -DLPD_SAFE_TEST_BUILD
-LOCAL_LDLIBS := -llog -landroid -ldl
+LOCAL_C_INCLUDES := \
+    $(LOCAL_PATH)
+
+LOCAL_CPPFLAGS := \
+    -std=c++17 \
+    -fexceptions \
+    -frtti \
+    -Wall \
+    -Wextra \
+    -DLPD_SAFE_TEST_BUILD
+
+LOCAL_LDLIBS := \
+    -llog \
+    -landroid \
+    -ldl
 
 include $(BUILD_SHARED_LIBRARY)
