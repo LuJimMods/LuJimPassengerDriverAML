488b160bc2193e95c2945d318fb7c1667e1042e0
LuJim <107133733+LuJimMods@users.noreply.github.com>
2026-06-25 13:03:08 -0300

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,19 +1,18 @@
-#include "PassengerDriver.h"
-#include "GameSymbols.h"
+#include "Config.h"
 #include "Log.h"
 
-#include <jni.h>
-#include <pthread.h>
-#include <unistd.h>
 #include <cstdio>
 #include <cstring>
 
-// Metadata para o AML reconhecer o mod
+// LuJim Passenger Driver - AML Safe Loader Test
+// Esta versao foi feita apenas para confirmar que a lib carrega no AML sem crash.
+// Nao inicia hooks, nao inicia thread e nao chama funcoes internas do GTA SA.
+
 struct ModVersionCompat {
-    unsigned short major = 1;
-    unsigned short minor = 0;
-    unsigned short revision = 0;
-    unsigned short build = 0;
+    unsigned short major;
+    unsigned short minor;
+    unsigned short revision;
+    unsigned short build;
 };
 
 struct ModInfoCompat {
@@ -26,82 +25,66 @@ struct ModInfoCompat {
 
 static ModInfoCompat gModInfo;
 static bool gModInfoReady = false;
-static bool gStarted = false;
+static bool gLoaded = false;
 
-static void LPD_Copy(char* dst, size_t size, const char* src) {
+static void LPD_Copy(char* dst, size_t size, const char* src)
+{
     if (!dst || size == 0) return;
     std::snprintf(dst, size, "%s", src ? src : "");
 }
 
-static void PrepareModInfo() {
+static void PrepareModInfo()
+{
     if (gModInfoReady) return;
+
+    std::memset(&gModInfo, 0, sizeof(gModInfo));
 
     LPD_Copy(gModInfo.szGUID, sizeof(gModInfo.szGUID), "lujim.passengerdriver");
     LPD_Copy(gModInfo.szName, sizeof(gModInfo.szName), "LuJim Passenger Driver");
-    LPD_Copy(gModInfo.szVersion, sizeof(gModInfo.szVersion), "1.0.0");
+    LPD_Copy(gModInfo.szVersion, sizeof(gModInfo.szVersion), "1.0.1-safe");
     LPD_Copy(gModInfo.szAuthor, sizeof(gModInfo.szAuthor), "LuJim Mods");
 
     gModInfo.version.major = 1;
     gModInfo.version.minor = 0;
-    gModInfo.version.revision = 0;
+    gModInfo.version.revision = 1;
     gModInfo.version.build = 0;
 
     gModInfoReady = true;
 }
 
-extern "C" __attribute__((visibility("default"))) void* __GetModInfo() {
+extern "C" __attribute__((visibility("default"))) void* __GetModInfo()
+{
     PrepareModInfo();
     return &gModInfo;
 }
 
-extern "C" __attribute__((visibility("default"))) const char* __INeedASpecificGame() {
+extern "C" __attribute__((visibility("default"))) const char* __INeedASpecificGame()
+{
     return "com.rockstargames.gtasa";
 }
 
-static void* LPD_Loop(void*) {
-    sleep(2);
-
-    while (true) {
-        PassengerDriver::Update(16.0f);
-        usleep(16000);
-    }
-
-    return nullptr;
-}
-
-extern "C" __attribute__((visibility("default"))) void OnModPreLoad() {
-    PrepareModInfo();
-    LPD_Log("[AML] OnModPreLoad chamado - LuJim Passenger Driver");
-}
-
-extern "C" __attribute__((visibility("default"))) void OnModLoad() {
-    PrepareModInfo();
-
-    LPD_Log("[AML] OnModLoad iniciado - LuJim Passenger Driver");
-
-    InitGameSymbols();
-    PassengerDriver::Init();
-
-    if (!gStarted) {
-        pthread_t thread;
-        pthread_create(&thread, nullptr, LPD_Loop, nullptr);
-        pthread_detach(thread);
-        gStarted = true;
-    }
-
-    LPD_Log("[AML] OnModLoad concluido - LuJim Passenger Driver carregado");
-}
-
-extern "C" __attribute__((visibility("default"))) void OnAllModsLoaded() {
-    LPD_Log("[AML] OnAllModsLoaded chamado - LuJim Passenger Driver");
-}
-
-__attribute__((constructor)) static void LPD_Constructor() {
+extern "C" __attribute__((visibility("default"))) void OnModPreLoad()
+{
     PrepareModInfo();
 }
 
-extern "C" __attribute__((visibility("default"))) jint JNI_OnLoad(JavaVM*, void*) {
+extern "C" __attribute__((visibility("default"))) void OnModLoad()
+{
     PrepareModInfo();
-    LPD_Log("[JNI] JNI_OnLoad chamado - LuJim Passenger Driver");
-    return JNI_VERSION_1_6;
+
+    if (gLoaded) return;
+    gLoaded = true;
+
+    Config::EnsureDefault();
+    Config::Load();
+
+    LPD_Log("[AML] LuJim Passenger Driver carregado com sucesso.");
+    LPD_Log("[AML] Versao: 1.0.1-safe");
+    LPD_Log("[AML] Caminho do INI: %s", Config::Path());
+    LPD_Log("[AML] Hooks desativados nesta versao segura.");
+}
+
+extern "C" __attribute__((visibility("default"))) void OnAllModsLoaded()
+{
+    LPD_Log("[AML] OnAllModsLoaded chamado - LuJim Passenger Driver.");
 }
