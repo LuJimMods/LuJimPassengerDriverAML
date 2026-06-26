8fc6e7b189f86dd148050a6b8063f169d27ab0ac
LuJim <107133733+LuJimMods@users.noreply.github.com>
2026-06-26 06:14:31 -0300

    Update Input.h

--- Input.h
+++ Input.h
@@ -1,13 +1,18 @@
 #pragma once
-class Input {
+
+class Input
+{
 public:
     static void Update(float dtMs);
     static bool ConsumeToggleRequest();
-    static void DebugSetPressed(bool p);
+    static void DebugSetPressed(bool pressed);
     static float HoldPercent();
+    static bool IsPressedNow();
+
 private:
     static bool toggleRequest;
     static bool lockedUntilRelease;
     static bool fakePressed;
     static float holdMs;
+    static bool lastPressed;
 };
